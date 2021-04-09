8d311a7f864f7c06c4326543ffe68499a74a9ac5
unknown <Akashpradhan-Git>
2021-04-10 03:46:27 +0530

    data change

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ window.addEventListener('load', () => {
     const to = localStorage.getItem('To')
 
 
-    if(From== 'bpd' && to == 'bbs'){
+    if (From == 'bpd' && to == 'bbs') {
         document.getElementById('From').innerText = "Baripada"
         document.getElementById('To').innerText = "Bhubaneswar"
     }
-    else if(From =='bls' && to =='bbs'){
+    else if (From == 'bls' && to == 'bbs') {
         document.getElementById('From').innerText = "Balasore"
         document.getElementById('To').innerText = "Bhubaneswar"
     }
 
-    
+
 
     document.getElementById('searchmore').addEventListener('click', () => (
         location.replace('index.html')
@@ -25,58 +25,73 @@ window.addEventListener('load', () => {
 
             const res = await fetch("data.json")
             const data = await res.json();
-            
+
             var out = ""
             data.map(e => {
                 f = e.from;
                 t = e.to;
-               
+
                 if (f == From && t == to) {
-                   
-                    
+
+
                     out +=
                         `
                        
-                    <li class="card">
-                    <div class="businfo">
-                        <div class="text">
-                            <div class="title">${e.name}</div>
-                            <div class="sub-title">${e.info}</div>
+                    
+                        <li class="card">
+
+                        <div class="businfo">
+                            <!-- FIXME: bus info -->
+                            <div class="text">
+                                <div class="title"><i class="fas fa-bus-alt"></i> ${e.name}</div>
+                                <div class="sub-title"><i class="fas fa-info-circle"></i> ${e.info}</div>
+                            </div>
+    
+                            <!-- FIXME: route info -->
+                            <div class="route">
+                                <div class="from"> ${e.fromName}</div>
+                                <div class="arrow"><i class="fas fa-route"></i></i></div>
+                                <div class="to"> ${e.toName}</div>
+                            </div>
+                            
+                            <!-- FIXME: price -->
+                            <div class="price">
+                                <div class="cut"> <del><small><sup>${e.price + 35}</sup></small></del></div>
+                                <i class="fas fa-rupee-sign"><i>${e.price}</i></i>
+                            </div>
+                        </div>
+    
+                        <!-- FIXME: other info -->
+                        <div class="otherinfo">
+                            <div class="seat"> ${e.seat}</div>
+                            <div class="time-taken"><i class="fas fa-hourglass-half"></i> ${e.totTime}</div>
+                            <div class="time">
+                                <div class="start"> <i class="far fa-clock"></i> ${e.start}</div>
+                                <div class="end"> ${e.end}</div>
+                            </div>
                         </div>
 
-                        <div class="route">
-                            <div class="from">${e.fromName}</div>
-                            <div class="arrow"><i class="fas fa-arrow-right"></i></div>
-                            <div class="to">${e.toName}</div>
+                        <div class="call">
+                            <i class="fas fa-headset"></i>
+                            <a href="tel:+917008061293">Book now!</a>
                         </div>
-                        <div class="price">
-                            <i class="fas fa-rupee-sign"> ${e.price}</i>
-                        </div>
-                    </div>
-                    <div class="otherinfo">
-                        <div class="seat">Total seat: ${e.seat}</div>
-                        <div class="time-taken">${e.totTime}</div>
-                        <div class="time">
-                            <div class="start">${e.start}</div>
-                            <div class="end">${e.end}</div>
-                        </div>
-                    </div>
-
-                </li>
+    
+                    </li>                
+ 
 
                    `
-                
+
                 }
-                  
+
             })
-         
+
             //console log
             console.log('Your Bus time table')
-            
+
             document.getElementById('output').innerHTML = out;
         }
         catch (error) {
-                console.error(error)
+            console.error(error)
         }
 
     }
--- style.js
+++ style.js
@@ -10,9 +10,9 @@ const navSlide = () =>{
 }
 navSlide();
 
-
+// set key and value to local storage
+//TODO: change to seassion staorage
 var f,t;
-
 document.addEventListener('DOMContentLoaded', () => {
 
     document.getElementById('select-from')
@@ -24,25 +24,28 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('search')
     .addEventListener('click', ()=>{
         
+        //set location in local storage
+
          localStorage.setItem('From',f)
          localStorage.setItem('To',t)
        
-        location.replace("https://www.google.com")
+       
     })
 })
 
 function  handleSelectFrom(){
     let select = document.getElementById('select-from')
     f = select.value;
-    console.log(select.value)
+   
 }
 
 function  handleSelectTo(){
     let select = document.getElementById('select-to')
     t  = select.value;
-    console.log(select.value)
+    
 }
 
+//TODO: page reload and set data to null
 
 window.addEventListener( "pageshow", function ( event ) {
   var historyTraversal = event.persisted || ( typeof window.performance != "undefined" && window.performance.navigation.type === 2 );
